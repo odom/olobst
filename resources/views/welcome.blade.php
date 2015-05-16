d6c8ba336a4a98de86c44aea8e2742b0c5874e4c
odom <bidam.neo@gmail.com>
2015-05-16 15:33:38 +0700

    [TASK] Under construction site.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,48 +3,100 @@
 		<title>Laravel</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-		<link href='{{asset('css/font-awesome.min.css')}}' rel='stylesheet' type='text/css'>
-        
+        <link href='{{asset('css/font-awesome.min.css')}}' rel='stylesheet' type='text/css'>
+        <link href='{{asset('css/app.css')}}' rel='stylesheet' type='text/css'>
+        <link href='{{asset('css/game.css')}}' rel='stylesheet' type='text/css'>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
 	</head>
 	<body>
 		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+            <br/>
+			<div class="row">
+                <div class="col-sm-6 col-sm-offset-3 text-center">
+                    <p class="alert alert-info"> Oop! The website is under construction. <i class="fa fa-cog fa-spin fa-2x"></i>
+                        <br/>
+                        <strong>
+                            The game below is use only CSS. So the feature is not full.
+                            Just for fun.
+                        </strong>
+                    </p>
+
+                </div>
 			</div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <form>
+                        <div class="board" style="overflow: hidden;">
+                            <div id="scorebar">Your scores:&nbsp;&nbsp;&nbsp;</div>
+                            <input id="bonus_01" name="radio1" class="bonus" type="radio">
+                            <input id="bonus_02" name="radio2" class="bonus" type="radio">
+                            <input id="bonus_03" name="radio3" class="bonus" type="radio">
+                            <input id="bonus_04" name="radio4" class="bonus" type="radio">
+                            <input id="bonus_05" name="radio5" class="bonus" type="radio">
+                            <input id="bonus_06" name="radio6" class="bonus" type="radio">
+                            <input id="bonus_07" name="radio7" class="bonus" type="radio">
+                            <input id="bonus_08" name="radio8" class="bonus" type="radio">
+                            <input id="bonus_09" name="radio9" class="bonus" type="radio">
+                            <input id="bonus_10" name="radio10" class="bonus" type="radio">
+                            <p class="enemy" id="enemy_01"></p>
+                            <p class="enemy" id="enemy_02"></p>
+                            <p class="enemy" id="enemy_03"></p>
+                            <p class="enemy enemy_t1" id="enemy_04"></p>
+                            <p class="enemy" id="enemy_05"></p>
+                            <p class="enemy" id="enemy_06"></p>
+                            <p class="enemy enemy_t3" id="enemy_07"></p>
+                            <p class="enemy" id="enemy_08"></p>
+                            <p class="enemy" id="enemy_09"></p>
+                            <p class="enemy" id="enemy_10"></p>
+                            <p class="enemy enemy_t1" id="enemy_11"></p>
+                            <p class="enemy" id="enemy_12"></p>
+                            <p class="enemy" id="enemy_13"></p>
+                            <p class="enemy enemy_t2" id="enemy_14"></p>
+                            <p class="enemy" id="enemy_15"></p>
+                            <p class="enemy" id="enemy_16"></p>
+                            <p class="enemy enemy_t3" id="enemy_17"></p>
+                            <p class="enemy" id="enemy_18"></p>
+                            <p class="enemy" id="enemy_19"></p>
+                            <p class="enemy enemy_t3" id="enemy_20"></p>
+                            <p class="enemy" id="enemy_21"></p>
+                            <p class="enemy" id="enemy_22"></p>
+                            <p class="enemy enemy_t1" id="enemy_23"></p>
+                            <p class="enemy" id="enemy_24"></p>
+                            <p class="enemy enemy_t2" id="enemy_25"></p>
+                            <p class="enemy" id="enemy_26"></p>
+                            <p class="enemy" id="enemy_27"></p>
+                            <p class="enemy enemy_t3" id="enemy_28"></p>
+                            <p class="enemy" id="enemy_29"></p>
+                            <p class="enemy" id="enemy_30"></p>
+                            <p class="enemy" id="enemy_31"></p>
+                            <p class="enemy" id="enemy_32"></p>
+                            <p class="enemy" id="enemy_33"></p>
+                            <p class="enemy" id="enemy_34"></p>
+                            <p class="enemy" id="enemy_35"></p>
+                            <p class="enemy" id="enemy_36"></p>
+                            <p class="enemy" id="enemy_37"></p>
+                            <p class="enemy" id="enemy_38"></p>
+                            <p class="enemy enemy_t3" id="enemy_39"></p>
+                            <p class="enemy" id="enemy_40"></p>
+                            <p class="enemy" id="enemy_41"></p>
+                            <p class="enemy" id="enemy_42"></p>
+                            <p class="enemy" id="enemy_43"></p>
+                            <p class="enemy enemy_t2" id="enemy_44"></p>
+                            <p class="enemy" id="enemy_45"></p>
+                            <p class="enemy" id="enemy_46"></p>
+                            <p class="enemy enemy_t1" id="enemy_47"></p>
+                            <p class="enemy" id="enemy_48"></p>
+                            <p class="enemy" id="enemy_49"></p>
+                            <p class="enemy" id="enemy_50"></p>
+
+                            <p id="status"><strong>Help:</strong> <span class="inactive">move cursor to the field.</span><span class="active">Ships fly around the halls, click on bonuses!</span></p>
+                        </div>
+
+                <audio src="http://nojsgame.majorov.su/assets/music/bg.mp3" loop autoplay></audio>
+                </form>
+                </div>
+            </div>
 		</div>
 	</body>
 </html>
